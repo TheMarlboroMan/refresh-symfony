0cccc6a474ab08b77695dce3174aa9272afe0512
Dani <marlborometal@gmail.com>
2018-02-08 01:18:13 +0100

    Removing some test code

--- src/AppBundle/Repository/PersonRepository.php
+++ src/AppBundle/Repository/PersonRepository.php
@@ -5,12 +5,7 @@ use Doctrine\ORM\EntityRepository;
 
 class PersonRepository extends EntityRepository {
 
-	private static $sum=1;
-
 	public function findAllNameLike($name) {
-
-		echo self::$sum;
-		++self::$sum;
 
 		$dql_string="SELECT p FROM AppBundle:Person p WHERE p.name LIKE :paramname ORDER by p.name ASC";
 
